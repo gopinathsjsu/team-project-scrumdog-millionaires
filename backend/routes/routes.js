5b3362895ef0e2689457d449adc335bbf981cef3
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-03-31 22:02:48 -0700

    login complete

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -22,13 +22,35 @@ const checkPersona = (personaType, res) => {
   }
 };
 
-try {
-  const { personaType, email, password } = req.body;
-  let table = await checkPersona(personaType, res);
-  if (!table) res.status(500).send("Persona not specified.");
-} catch (e) {
-  console.error(e);
-  res.status(500).json({
-    error: "Internal Server Error: Please try again",
-  });
-}
+router.post("/login", async (req, res, next) => {
+  try {
+    const { personaType, email, password } = req.body;
+    let table = await checkPersona(personaType, res);
+    if (!table) res.status(500).send("Persona not specified.");
+    else {
+      // Invoke the query
+      const results = await apiModel.login(table, email);
+      if (results.length > 0) {
+        if (hashedPassword(password) === results[0].password) {
+          // Return the response
+          res.json(JSON.parse(JSON.stringify(results[0])));
+        } else {
+          // Auth Error
+          res.status(401).json({
+            error: "Incorrect Password",
+          });
+        }
+      } else {
+        // Auth Error
+        res.status(401).json({
+          error: "Incorrect Email or Password",
+        });
+      }
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      error: "Internal Server Error: Please try again",
+    });
+  }
+});
