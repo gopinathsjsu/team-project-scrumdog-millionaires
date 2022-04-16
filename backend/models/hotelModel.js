688b1cb02442a6d09c440d163018a87d851f8368
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-04-15 22:08:07 -0700

    hotel Model cont

--- backend/models/hotelModel.js
+++ backend/models/hotelModel.js
@@ -14,3 +14,25 @@ model.getHotels = (table = DB_TABLE_HOTEL) => {
     });
   });
 };
+
+model.getHotelsByID = (hotel_id, table = DB_TABLE_HOTEL) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+			SELECT * 
+			FROM ${table}
+			WHERE id = ${hotel_id}
+		`;
+    db.query(query, (err, hotel) => {
+      if (err) return reject(err);
+      return resolve(hotel);
+    });
+  });
+};
+
+model.updateRequestactory = (name) => {
+  // ADDING extra SPACE after sentence 1 to delimit fields
+  const _ = `
+		name = '${name}'
+	`;
+  return _;
+};
