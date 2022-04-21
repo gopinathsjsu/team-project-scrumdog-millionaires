2e802e5fce63562c153f8d214be9bfeb29c25faa
abhishek ganesan <abhishek.ganesan@sjsu.edu>
2022-04-21 14:03:50 -0700

    Hotel login frontend

---
+++ frontend/src/userLogin/LoginForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "LoginForm.css";
+
+const LoginForm = () => {
+  return (
+    <div className="div-6">
+      <span className="span-4">{"Email"}</span>
+      <div className="textfield-7">
+        <span className="span-5">{"Enter User Email here..."}</span>
+      </div>
+      <span className="span-6">{"Password"}</span>
+      <div className="textfield-password-8">
+        <span className="span-7">{"Enter User Password here..."}</span>
+      </div>
+      <span className="span-8">{"Forgot password?"}</span>
+    </div>
+  );
+};
+export default LoginForm;
