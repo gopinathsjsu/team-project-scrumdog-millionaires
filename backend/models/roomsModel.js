b8df698417e3e32cc74c6eaa5e9ba8c9d7b48a98
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-04-22 22:21:09 -0700

    rooms model get room

--- backend/models/roomsModel.js
+++ backend/models/roomsModel.js
@@ -7,3 +7,33 @@ model.ROOM_ACCOMODATION = {
   double: 2,
   suites: 4,
 };
+
+model.getRoomsByRoomID = (roomId, table = DB_TABLE_ROOMS) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+			SELECT *
+			FROM ${table}
+			WHERE id = '${roomId}'
+			`;
+    console.info("Query = ", query);
+    db.query(query, (err, results) => {
+      if (err) return reject(err);
+      return resolve(results);
+    });
+  });
+};
+
+model.getRooms = (table = DB_TABLE_ROOMS) => {
+  return new Promise((resolve, reject) => {
+    const query = `
+			SELECT * 
+			FROM ${table}
+		`;
+    console.info("QUERY =", query);
+
+    db.query(query, (err, hotel) => {
+      if (err) return reject(err);
+      return resolve(hotel);
+    });
+  });
+};
