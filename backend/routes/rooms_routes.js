ef41db7c3959485391f9ab87f2a7bf9555549425
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-05-11 10:32:24 -0700

    room routes init

---
+++ backend/routes/rooms_routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const SSecurity = require("../services/SimpleSecurity");
+const router = express.Router();
+const RoomService = require("./../services/room_service");
+const room_service = new RoomService();
+
+const PricingService = require("./../services/pricing_service");
+const pricing_service = new PricingService();
+
+router.get("/:room_id?", async (request, response) => {
+  const { params } = request;
+  const { room_id } = params;
+  try {
+    const { status, ...data } = await room_service.getRooms(room_id);
+    return response.status(status).send({ ...data });
+  } catch (err) {
+    console.error(`HotelRoutes::GET /rooms:: Internal server error \n${err}`);
+    return response.status(500).send({ msg: "Internal  Server Error" });
+  }
+});
