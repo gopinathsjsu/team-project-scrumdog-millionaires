c5fe657950eac146f7098c7a5352b4f5fe932ebe
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-05-12 10:32:44 -0700

    room routes admin init

--- backend/routes/rooms_routes.js
+++ backend/routes/rooms_routes.js
@@ -18,3 +18,17 @@ router.get("/:room_id?", async (request, response) => {
     return response.status(500).send({ msg: "Internal  Server Error" });
   }
 });
+
+router.use("/", SSecurity.authenticate_admin);
+
+router.post("/", async (request, response) => {
+  const { params, body } = request;
+  const { hotel_id } = params;
+  try {
+    const { status, ...data } = await room_service.createRoom(body);
+    return response.status(status).send({ ...data });
+  } catch (err) {
+    console.error(`HotelRoutes::POST /rooms:: Internal server error \n${err}`);
+    return response.status(500).send({ msg: "Internal  Server Error" });
+  }
+});
