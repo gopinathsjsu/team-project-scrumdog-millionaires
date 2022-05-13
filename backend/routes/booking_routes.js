45b62f15882c3ff0b32f95acfb09e040325b4956
Rooppesh <rooppesh.sarankapani@gmail.com>
2022-05-13 10:34:05 -0700

    booking routes crud

--- backend/routes/booking_routes.js
+++ backend/routes/booking_routes.js
@@ -18,3 +18,41 @@ router.get("/users/:userId", async (request, response) => {
     return response.status(500).send({ msg: "Internal  Server Error" });
   }
 });
+
+router.post("/rooms/:room_id", async (request, response) => {
+  const { params, body } = request;
+  const { room_id } = params;
+  console.log(body);
+  try {
+    const { status, ...data } = await booking_service.bookRoomForUser(
+      room_id,
+      body
+    );
+    return response.status(status).send({ ...data });
+  } catch (err) {
+    console.error(
+      `BookingRoutes::POST /bookings/hotels/${hotel_id}/rooms/${room_id}:: Internal server error \n ${err}`
+    );
+    return response.status(500).send({ msg: "Internal  Server Error" });
+  }
+});
+
+// Update booking as a user
+router.put("/:booking_id", async (request, response) => {
+  const { query, params, body } = request;
+  const { userId } = query;
+  const { booking_id } = params;
+  try {
+    const { status, ...data } = await booking_service.updateBooking(
+      userId,
+      booking_id,
+      body
+    );
+    return response.status(status).send({ ...data });
+  } catch (err) {
+    console.error(
+      `BookingRoutes::PUT /bookings/${booking_id}:: Internal server error \n ${err}`
+    );
+    return response.status(500).send({ msg: "Internal  Server Error" });
+  }
+});
